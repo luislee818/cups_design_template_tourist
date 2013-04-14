b146497cf2d5fad1230158c104822188f743e9c7
Dapeng Li <luislee818@gmail.com>
2013-04-14 20:38:13 +0800

    Support switching pages

--- lib/tourist.rb
+++ lib/tourist.rb
@@ -1,8 +1,10 @@
 require 'selenium-webdriver'
+require 'pry'
 
 class Tourist
 	POPUP_WIDTH = 900  # pixels
 	WAIT = 30  # seconds
+	DESIGN_TEMPLATES_PER_PAGE = 21
 	IFRAME_POPUP_ID = 'iframePopUp'
 	DESIGN_TEMPLATE_ID_REGEX = /[[:alnum:]]{8}-[[:alnum:]]{4}-[[:alnum:]]{4}-[[:alnum:]]{4}-[[:alnum:]]{12}/
 
@@ -14,8 +16,12 @@ class Tourist
 
 		@driver = Selenium::WebDriver.for @browser
 		@wait = Selenium::WebDriver::Wait.new(:timeout => WAIT)
-		@index_start = 8
-		@index_end = 12
+		@page_start = 4
+		@page_end = 5
+		@max_page = -1
+		@current_page = @page_start
+		@index_start = 6
+		@index_end = [8, DESIGN_TEMPLATES_PER_PAGE].min
 		@current_index = @index_start
 		@first_spot = true
 	end
@@ -23,16 +29,57 @@ class Tourist
 	def begin_tour
 		Dir.mkdir(@dir) unless File.directory?(@dir)
 
-		while @current_index <= @index_end
-			go_to_next_spot
+		while @current_page <= @page_end
+			while @current_index <= @index_end
+				template_id = go_to_spot(@current_page, @current_index)
 
-			@current_index += 1
+				take_shot(@current_page, @current_index, template_id)
+
+				@current_index += 1
+			end
+
+			@current_index = @index_start
+			@current_page += 1
 		end
 
 		end_tour
 	end
 
-	def	wait_for_design_template_popup()
+	def go_to_spot(page, index)
+		if @first_spot
+			navigate_to_page
+			@first_spot = false
+		else
+			click_change_design_link
+		end
+
+		wait_for_design_template_popup
+		switch_to_popup_iframe
+
+		save_max_page_number if @max_page == -1
+		go_to_page page unless page == 1
+
+		template_id = select_design_template index
+		switch_to_main_content
+		wait_for_all_elements_to_show
+
+		template_id
+	end
+
+	def navigate_to_page
+		puts '[Init] Navigating to page'
+		@driver.navigate.to @url
+		puts '[Init] Navigated to page'
+	end
+
+	def click_change_design_link
+		puts '[Change Design] Clicking change design link'
+		change_design_link = @driver.find_element(:link_text, 'Change Design')
+		change_design_link.click
+		puts '[Change Design] Clicked change design link'
+	end
+
+	def	wait_for_design_template_popup
 		puts '[Wait] Waiting for popup'
 		iframe_popup = @driver.find_element(:id, 'iframePopUp')
 		@wait.until { iframe_popup.size.width == POPUP_WIDTH }
@@ -43,6 +90,43 @@ class Tourist
 		puts '[Switch] Switching to popup iframe'
 		@driver.switch_to.frame IFRAME_POPUP_ID
 		puts '[Switch] Popup iframe shown'
+	end
+
+	def save_max_page_number
+		puts '[Pager] Looking for maximum page number'
+		page_number_input = @driver.find_element(:css, '#dtPageOf #pageOfValue input')
+		@max_page = page_number_input.attribute('value').to_i
+		puts "[Pager] Maximum page number saved as #{@max_page}"
+	end
+
+	def go_to_page(number)
+		old_onclick = get_first_design_template_on_page().attribute 'onclick'
+		puts '[Pager] Record old onclick value'
+		page_index_input = @driver.find_element(:css, '#dtPageTitle input')
+		page_index_input.send_keys :backspace, :backspace, number.to_s, :return
+		puts '[Pager] Send return key, begin waiting for first template to change'
+		@wait.until do
+			begin
+				get_first_design_template_on_page().attribute('onclick') != old_onclick
+			rescue
+				false
+			end
+		end
+		puts '[Pager] First element changed'
+	end
+
+	def select_design_template(index)
+		click_targets = @driver.find_elements(:css, '.dtTemplates input')
+		target = click_targets[index]
+		onclick = target.attribute 'onclick'
+		template_id = onclick.scan(DESIGN_TEMPLATE_ID_REGEX)[1]
+		target.click
+
+		template_id
+	end
+
+	def get_first_design_template_on_page
+		@driver.find_elements(:css, '.dtTemplates input')[0]
 	end
 
 	def switch_to_main_content
@@ -57,51 +141,11 @@ class Tourist
 		puts '[Wait] All elements shown'
 	end
 
-	def click_change_design_link
-		puts '[Change Design] Clicking change design link'
-		change_design_link = @driver.find_element(:link_text, 'Change Design')
-		change_design_link.click
-		puts '[Change Design] Clicked change design link'
-	end
-
-	def take_shot(index, template_id)
-		shot_name = "#{index}. #{template_id}.png"
+	def take_shot(page, index, template_id)
+		shot_name = "p#{page} i#{index}.#{template_id}.png"
 		puts "[Shot] Taking shot for #{shot_name}"
 		@driver.save_screenshot "./#{@dir}/#{shot_name}"
 		puts "[Shot] Shot taken"
-	end
-
-	def navigate_to_page
-		puts '[Init] Navigating to page'
-		@driver.navigate.to @url
-		puts '[Init] Navigated to page'
-	end
-
-	def select_design_template(index)
-		click_targets = @driver.find_elements(:css, '.dtTemplates input')
-		target = click_targets[index]
-		onclick = target.attribute 'onclick'
-		template_id = onclick.scan(DESIGN_TEMPLATE_ID_REGEX)[1]
-		target.click
-
-		template_id
-	end
-
-	def go_to_next_spot
-		if @first_spot
-			navigate_to_page
-			@first_spot = false
-		else
-			click_change_design_link
-		end
-
-		wait_for_design_template_popup()
-		switch_to_popup_iframe
-		template_id = select_design_template @current_index
-		switch_to_main_content
-		wait_for_all_elements_to_show
-
-		take_shot(@current_index, template_id)
 	end
 
 	def end_tour
